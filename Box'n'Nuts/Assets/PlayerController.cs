bd346f2f037591e9d7c7bb014d33ec37079cccc4
Metalynx <casper@clausfh.dk>
2017-04-22 01:27:39 +0200

    no message

---
+++ Box'n'Nuts/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    int playerNum;
+    float speed = 0.05f;
+    Vector3 moveDir;
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Vertical") > 0)
+        { 
+            moveDir.y += Input.GetAxis("Vertical");
+        }
+        moveDir.x += (Input.GetAxis("Horizontal") / 10);
+        moveDir.Normalize();
+
+        this.transform.position = this.transform.position + (moveDir * speed);
+        if (this.transform.position.z != 0)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y,0);
+        }
+
+        RaycastHit hit;
+        Physics.Raycast(transform.position, new Vector3(0, -1, 0),out hit, 5);
+        
+
+	}
+}
